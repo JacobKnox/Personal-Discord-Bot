d78e7fb65c7a78da5a464ab76b98ce7245764eb9
Jacob Knox <jacobknoxa@gmail.com>
2022-11-21 21:53:41 -0500

    Initial commit

---
+++ knoxbot.py
@@ -0,0 +1,123 @@
+from os import getenv as ENV
+from os.path import join, dirname
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+from pnw_utils import *
+import pnwkit
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+TOKEN = ENV("DISCORD_TOKEN")
+GUILD = ENV('DISCORD_GUILD')
+API_KEY = ENV("API_KEY")
+
+# create a QueryKit with my API key to create queries
+kit = pnwkit.QueryKit(API_KEY)
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+        
+# Add a command to calculate the cost of infrastructure
+@bot.command(name='infra')
+async def calc_infra(ctx, *args):
+    # If there are less than two arguments, then it isn't a valid command call
+    if len(args) < 2:
+        await ctx.send("You have not specified enough arguments for this command!")
+    # If there are two arguments, then just calculate the difference between the two
+    elif len(args) == 2:
+        infra_cost = calculate_infrastructure_value(float(args[0]), float(args[1]))
+        embed=discord.Embed(title="Calculate Infrastructure", description=f'The cost to go from {args[0]} to {args[1]} is:\n${infra_cost: ,.2f}', color=0xFF5733)
+        await ctx.send(embed=embed)
+    # If there are three arguments, then calculate the difference between the two considering their nation
+    elif len(args) == 3:
+        # Assemble the query with their nation ID
+        query = kit.query(
+            "nations", {
+                "id": int(args[0]),
+                "first": 1
+            },
+            """
+            domestic_policy
+            nation_name
+            government_support_agency
+            center_for_civil_engineering
+            advanced_engineering_corps
+            """)
+        # Process the above nation query
+        result = query.get()
+        infra_cost = calc_infra_cost(result, float(args[1]), float(args[2]))
+        embed=discord.Embed(title="Calculate Infrastructure", description=f'The cost to go from {args[1]: ,.2f} to {args[2]: ,.2f} for [{result.nations[0].nation_name}](https://politicsandwar.com/nation/id={args[0]}) is:\n${infra_cost: ,.2f}', color=0xFF5733)
+        await ctx.send(embed=embed)
+    # If there are more than three arguments, then there's no matching command
+    else:
+        await ctx.send("You've specified too many arguments!")
+    
+@bot.command(name='city')
+async def calc_city(ctx, nation_id, end):
+    query = kit.query(
+        "nations", {
+            "id": int(nation_id),
+            "first": 1
+        },
+        """
+        cities{
+            farm
+            land
+        }
+        defensive_wars
+        offensive_wars
+        soldiers
+        population
+        continent
+        resource_production_center
+        massirr
+        """)
+    # process the above nation query
+    result = query.get()
+    city_cost = calc_city_cost(result, int(end))
+    await ctx.send(f'${city_cost: ,.2f}')
+
+@bot.command(name="food")
+async def calc_food(ctx, nation_id):
+    query = kit.query(
+        "nations", {
+            "id": int(nation_id),
+            "first": 1,
+        },
+        """
+        cities{
+            farm
+            land
+        }
+        defensive_wars{
+        }
+        offensive_wars{
+        }
+        soldiers
+        nation_name
+        population
+        continent
+        resource_production_center
+        massirr
+        """)
+    result = query.get()
+    net_food, food_production, food_usage = calc_food_rev(result)
+    embed=discord.Embed(title="Food Statistics", description=f'Statistics about food revenue for [{result.nations[0].nation_name}](https://politicsandwar.com/nation/id={nation_id}):\nProduction: {abs(food_production): ,.2f}\nUsage: {food_usage: ,.2f}\nNet: {net_food: ,.2f}', color=0xFF5733)
+    await ctx.send(embed=embed)
+
+bot.run(TOKEN)
---
+++ pnw_utils.py
@@ -0,0 +1,273 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import pnwkit
+import math
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# save my API key in a variable
+API_KEY = os.getenv("API_KEY")
+RESOURCES  = {
+    "af": ["oil", "bauxite", "uranium"],
+    "an": ["oil", "coal", "uranium"],
+    "as": ["oil", "iron", "uranium"],
+    "au": ["coal", "bauxite", "lead"],
+    "eu": ["coal", "iron", "lead"],
+    "na": ["coal", "iron", "uranium"],
+    "sa": ["oil", "bauxite", "lead"]
+}
+
+# create a QueryKit with my API key to create queries
+kit = pnwkit.QueryKit(API_KEY)
+
+# "Test" API call to get a bunch of information
+general_query = kit.query(
+    "nations", {
+        "id": 244934,
+        "first": 1
+    }, """
+	population
+	soldiers
+	continent
+	defensive_wars{
+		turns_left
+	}
+	offensive_wars{
+		turns_left
+	}
+	cities{
+		farm
+		land
+		coal_mine
+		steel_mill
+		powered
+		infrastructure
+		coal_power
+	}
+	massirr
+	advanced_urban_planning
+	urban_planning
+	domestic_policy
+	government_support_agency
+	center_for_civil_engineering
+	advanced_engineering_corps
+	iron_works
+    resource_production_center
+	""")
+# process the above nation query
+general_result = general_query.get()
+
+### FUNCTIONS ###
+
+
+# function for calculating the daily food revenue of a nation
+# production accurate within a few ones or tens, usage accurate within a few thousands?
+def calc_food_rev(api_result):
+    nation = api_result.nations[0]
+    # querying radiation information from GameInfo
+    radiation_query = kit.query(
+        "game_info", {}, """
+		game_date
+		radiation {
+			africa
+			antarctica
+			asia
+			australia
+			europe
+			global
+			north_america
+			south_america
+		}
+		""")
+    # attempting to process above query
+    radiation_result = radiation_query.get()
+    RADIATION = {
+        "af": radiation_result.game_info.radiation.africa,
+        "an": radiation_result.game_info.radiation.antarctica,
+        "as": radiation_result.game_info.radiation.asia,
+        "au": radiation_result.game_info.radiation.australia,
+        "eu": radiation_result.game_info.radiation.europe,
+        "na": radiation_result.game_info.radiation.north_america,
+        "sa": radiation_result.game_info.radiation.south_america
+    }
+    
+    # intialize the food_usage variable with the usage from population
+    food_usage = nation.population / 1000
+    # if the nation is in any wars
+    if (len(nation.defensive_wars) > 0 or len(nation.offensive_wars) > 0):
+        # add the usage from soldiers in wartime
+        food_usage += nation.soldiers / 500
+    else:
+        # add the usage from soldiers in peacetime
+        food_usage += nation.soldiers / 750
+    # initialize the food_production variable to 0
+    food_production = 0
+    if(nation.resource_production_center and len(nation.cities) < 16):
+        food_production += (1 + math.floor(min(len(nation.cities), 10) / 2)) * 12
+    # iterate over all cities in the nation
+    for city in nation.cities:
+        # initialize the city_production variable for that city to 0
+        city_production = 0
+        # if the nation has the Mass Irrigation national project
+        if (nation.massirr):
+            # set the production to the number of farms multiplied by the land divided by 400 (effect of Mass Irrigation)
+            city_production = city.farm * 12 * (city.land / 400)
+        else:
+            # set the production to the number of farms multiplied by the land divided by 500 (default production)
+            city_production = city.farm * 12 * (city.land / 500)
+        # if the nation has more than one farm
+        if (city.farm > 1):
+            # apply the production bonus
+            city_production *= 1 + (city.farm - 1) * 0.0263157894737
+        # add the current city's production to the nation's total production
+        food_production += city_production
+    # the seasonal affect on food production (need to check what continent and season the nation is on)
+    season_affect = 1
+    # save the continent into a variable
+    continent = nation.continent
+    continent_radiation = RADIATION[continent]
+    # if they're on North America, Europe, or Asia
+    if (continent in ["na", "eu", "as"]):
+        # summer
+        if (radiation_result.game_info.game_date.month > 5 and radiation_result.game_info.game_date.month < 9):
+            season_affect = 1.2
+        # winter
+        else:
+            season_affect = 0.8
+    # if they're on South America, Africa, or Australia
+    elif (continent in ["sa", "af", "au"]):
+        # winter
+        if (radiation_result.game_info.game_date.month > 5 and radiation_result.game_info.game_date.month < 9):
+            season_affect = 0.8
+        # summer
+        else:
+            season_affect = 1.2
+    # if they're on Antarctica
+    else:
+        # Antarctica is unaffected by season, but gets a permanent -50% food production
+        season_affect = 0.5
+    # calculate the radiation factor on food production
+    radiationFactor = 1 - ((continent_radiation + radiation_result.game_info.radiation.global_) / 1000)
+    # apply the seasonal factor to the total production
+    food_production *= season_affect
+    # apply the radiation factor to the total production
+    food_production *= radiationFactor
+    # return the difference between the food_production and food_usage to determine net food revenue
+    return round(food_production - food_usage, 2), food_production, food_usage
+
+
+# function for calculating the cost of bringing a nation from their current city count to a goal
+# completely accurate
+def calc_city_cost(nation_call, goal_city):
+    # intialize a temporary total cost to 0
+    total_cost = 0
+    # intialize a temporary cost to 0
+    city_cost = 0
+    for city_num in range(len(nation_call.nations[0].cities), goal_city):
+        # calculate the cost of the next city
+        city_cost = (50000 * pow(
+            (city_num - 1), 3) + 150000 * city_num + 75000)
+        # if the nation has Urban Planning project, apply it
+        if (nation_call.nations[0].urban_planning == True):
+            city_cost -= 50000000
+        # if the nation has Advanced Urban Planning project, apply it
+        if (nation_call.nations[0].advanced_urban_planning == True):
+            city_cost -= 100000000
+        # if the nation's domestic policy is currently Manifest Destiny, apply it
+        if (nation_call.nations[0].domestic_policy == pnwkit.data.DomesticPolicy(1)):
+            # if the nation has Government Support Agency project, then couple its effects with Manifest Destiny
+            if (nation_call.nations[0].government_support_agency == True):
+                city_cost *= 0.925
+            # otherwise, just apply Manifest Destiny
+            else:
+                city_cost *= 0.95
+        # add the cost of the next city to the total cost
+        total_cost += city_cost
+    # finally, return the total city cost
+    return total_cost
+
+
+# function to calculate the cost of buying infra from a current amount to a goal amount
+# accurate within a few tens or ones for multiples of 100, but within a few thousands for non-multiples of 100
+def calc_infra_cost(nation_call, current_infra, goal_infra):
+    infra_cost = calculate_infrastructure_value(current_infra, goal_infra)
+    nation = nation_call.nations[0]
+    if (infra_cost > 0):
+        modifier = 1
+        if(nation.center_for_civil_engineering):
+            modifier -= 0.05
+            if(nation.advanced_engineering_corps):
+                modifier -= 0.05
+        if (nation.domestic_policy == pnwkit.data.DomesticPolicy(5) and nation.government_support_agency):
+            modifier -= 0.075
+        elif (nation.domestic_policy == pnwkit.data.DomesticPolicy(5)):
+            modifier -= 0.05
+        infra_cost *= modifier
+    return round(infra_cost, 2)
+
+
+def calc_coal_rev(nation_call):
+    nation = nation_call.nations[0]
+    # initialize helper variables for production and usage to 0
+    coal_production = 0
+    mill_usage = 0
+    power_usage = 0
+    if(nation.resource_production_center and "coal" in RESOURCES[nation.continent] and len(nation.cities) < 16):
+        coal_production += (1 + math.floor(min(len(nation.cities), 10) / 2)) * 12
+    # loop over each city in the nation
+    for city in nation.cities:
+        # calculate its coal production
+        city_coal = city.coal_mine * 3
+        city_coal *= (1 + ((city.coal_mine - 1) * 0.055555555555))
+        coal_production += city_coal
+        city_mill = city.steel_mill * 3
+        city_mill *= (1 + ((city.steel_mill - 1) * 0.125))
+        if (nation.iron_works):
+            city_mill *= 1.36
+        mill_usage += city_mill
+        if (city.powered and city.coal_power > 0):
+            temp_infra = city.infrastructure
+            for _ in range(0, city.coal_power):
+                if (temp_infra >= 500):
+                    power_usage += 6
+                    temp_infra -= 500
+                elif (temp_infra > 0):
+                    power_usage += (math.ceil(temp_infra) / 100) * 1.2
+                    temp_infra = 0
+    return round(coal_production - mill_usage - power_usage, 2)
+
+
+### The following code is taken directly from the open source Rift project ###
+### (https://github.com/mrvillage/rift/blob/master/bot/src/funcs/tools.py) ###
+def infrastructure_price(amount: float, /) -> float:
+    return ((abs(amount - 10) ** 2.2) / 710) + 300
+
+
+def calculate_infrastructure_value(start: float, end: float, /) -> float:
+    value = 0
+    start = round(start, 2)
+    end = round(end, 2)
+    difference = end - start
+    if not difference:
+        return 0
+    if difference < 0:
+        return 150 * difference
+    if difference > 100 and difference % 100 == 0:
+        chunk = round(infrastructure_price(start), 2) * 100
+        return value + chunk + calculate_infrastructure_value(start + 100, end)
+    if difference > 100 and difference % 100 != 0:
+        chunk = round(infrastructure_price(start), 2) * (difference % 100)
+        return (
+            value
+            + chunk
+            + calculate_infrastructure_value(start + difference % 100, end)
+        )
+    if difference <= 100:
+        chunk = round(infrastructure_price(start), 2) * difference
+        return value + chunk
+    return value
+
+
+### End of code from Rift ###
